016fc283e5f4db1f5edc8efabc0a5a4c8b27d3e2
rafae <rafaey1995@gmail.com>
2024-11-20 19:22:58 -0600

    Le agregue cositas nuevas xd

--- PruebaCalidad/src/main/java/com/fca/calidad/maven/App.java
+++ PruebaCalidad/src/main/java/com/fca/calidad/maven/App.java
@@ -13,5 +13,17 @@ public class App {
         
         DAOUserSQLite dao = new DAOUserSQLite();
         User usuario = new User("nombre","correo","password");
-        int id = dao.save(usuario);    }
+        int id = dao.save(usuario);    
+    
+    	usuario.setId(id);
+    	
+    	//find
+    	System.out.println(dao.findById(id).toString());
+    	
+    	usuario.setPassword("12345678");
+    	dao.updateUser(usuario);
+    	System.out.println(dao.updateUser(usuario));
+    	
+    	dao.deleteById(1);
+    }
 }
